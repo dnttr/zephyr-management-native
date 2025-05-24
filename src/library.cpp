bf37771ca73f6b6936b14d8cabeb54bb66d2a9ae
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-24 18:03:15 +0200

    Update CMake configuration to include additional include directories and link libraries; add JNI_onload function in library.cpp

--- src/library.cpp
+++ src/library.cpp
@@ -1,3 +1,12 @@
 //
 // Created by Damian Netter on 23/05/2025.
 //
+
+#include <iostream>
+#include <jni.h>
+
+int JNI_onload(JavaVM *vm, void *reserved) {
+    //dlopen czy cos?
+    std::cout << "XD" << std::endl;
+    return JNI_VERSION_21;
+}
